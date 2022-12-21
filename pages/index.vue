4f9eee8cbcc576ae9cd15b448c3e1c1344d581e9
Jure Topolak <jure.topolak@gmail.com>
2022-12-21 23:05:34 +0100

    Update test pages.

--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,12 @@
 
 <template>
   <div>
-    <v-btn color="test1" variant="flat" class="m-1">
+    <div>Index</div>
+    <v-btn color="test1" variant="flat" class="m-1 ml-0">
       Test1
     </v-btn>
     <v-btn color="test2" variant="flat" class="m-1">
       Test2
     </v-btn>
-    <v-avatar color="test1" size="x-large" class="m-1">
-      T1
-    </v-avatar>
-    <v-avatar color="test2" size="x-large" class="m-1">
-      T2
-    </v-avatar>
   </div>
 </template>
